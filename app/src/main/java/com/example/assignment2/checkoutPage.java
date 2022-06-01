1b67832dced1c46408ca646ed21bcdb63f2eb3b8
khalid khalil <kalidkalil4254@gmail.com>
2022-06-01 02:26:23 +0200

    f

--- app/src/main/java/com/example/assignment2/checkoutPage.java
+++ app/src/main/java/com/example/assignment2/checkoutPage.java
@@ -1,6 +1,14 @@
 package com.example.assignment2;
 
+import android.Manifest;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.*;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.KeyEvent;
@@ -9,8 +17,16 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.*;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -23,11 +39,16 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class checkoutPage extends AppCompatActivity {
+public class checkoutPage extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
 
-    ImageView search_action_btn,menu,home,fav,cart,nav;
-    EditText search_action_txt;
+    Button btn;
+    private final static int PLACE_PICKER_REQUEST = 999;
+    private final static int LOCATION_REQUEST_CODE = 23;
+
+    ImageView menu,home,fav,cart,nav;
     protected  static  final  int RESULT_SPEECH=1;
     ImageView mic;
     TextView total;
@@ -37,14 +58,28 @@ public class checkoutPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkout);
 
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+
+
+
+
+
         total=findViewById(R.id.totalPrice_cartPage_txt);
         nav=findViewById(R.id.navBar);
         home=findViewById(R.id.homePage);
         fav=findViewById(R.id.favPage);
         cart=findViewById(R.id.cartPage);
         menu=findViewById(R.id.menuPage);
-        search_action_txt=findViewById(R.id.search_actionbar_txt);
-        search_action_btn=findViewById(R.id.search_action_btn);
+
 
 
 
@@ -68,43 +103,137 @@ public class checkoutPage extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(),cartPage.class));
         });
         menu.setOnClickListener((e)->{
-            startActivity(new Intent(getApplicationContext(), com.example.assignment2.menu.class));
+            startActivity(new Intent(getApplicationContext(), menu.class));
         });
 
-
-        search_action_btn.setOnClickListener((e)->{
-            String search_txt=search_action_txt.getText().toString();
-            // creating a intent
-            Intent intent = new Intent(getApplicationContext(), products.class);
-            Bundle bundle = new Bundle();
-            bundle.putString("search", search_txt);
-            intent.putExtras(bundle);
-            startActivity(intent);
-            Toast.makeText(getApplicationContext(),"Searching:"+search_txt,Toast.LENGTH_SHORT).show();
-        });
-
-        search_action_txt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_NEXT || actionId == EditorInfo.IME_ACTION_DONE) {
-                    String search_txt=search_action_txt.getText().toString();
-                    // creating a intent
-                    Intent intent = new Intent(getApplicationContext(), products.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("search", search_txt);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(),"Searching:"+search_txt,Toast.LENGTH_SHORT).show();
-                    handled = true;
-                }
-                return handled;
-            }
-        });
 
 
 
     }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    mMap.setMyLocationEnabled(true);
+                    mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                        @Override
+                        public void onMyLocationChange(Location location) {
+                            LatLng ltlng = new LatLng(location.getLatitude(), location.getLongitude());
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                                    ltlng, 16f);
+                            mMap.animateCamera(cameraUpdate);
+                        }
+                    });
+                    Location location = mMap.getMyLocation();
+
+                    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                        @Override
+                        public void onMapClick(LatLng latLng) {
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            markerOptions.position(latLng);
+
+                            markerOptions.title(getAddress(latLng));
+                            mMap.clear();
+                            CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
+                                    latLng, 15);
+                            mMap.animateCamera(location);
+                            mMap.addMarker(markerOptions);
+                        }
+                    });
+
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+
+    private String getAddress(LatLng latLng){
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+            if (prev != null) {
+
+                ft.remove(prev);
+            }
+            ft.addToBackStack(null);
+            DialogFragment dialogFragment = new ConfirmAddress();
+
+            Bundle args = new Bundle();
+            args.putDouble("lat", latLng.latitude);
+            args.putDouble("long", latLng.longitude);
+            args.putString("address", address);
+            dialogFragment.setArguments(args);
+            dialogFragment.show(ft, "dialog");
+            return address;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "No Address Found";
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -163,20 +292,4 @@ public class checkoutPage extends AppCompatActivity {
         }
     }
 
-
-
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case RESULT_SPEECH:
-                if(resultCode==RESULT_OK && data!=null) {
-                    ArrayList<String> text=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    search_action_txt.setText(text.get(0));
-                }
-                break;
-        }
-    }
 }
